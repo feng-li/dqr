eec45b84661adad3bfc53c9783546adaa8b8f3d7
Feng Li <m@feng.li>
2021-04-19 17:03:52 +0800

    update communication cost

--- projects/commcost_plot.R
+++ projects/commcost_plot.R
@@ -1,9 +1,31 @@
-
 commcost = read.table("commcost_data.csv", header = TRUE)
-par(ps = 12, cex = 1, cex.main = 1)
-plot(commcost$sample_size, commcost$time_commcost,
-     xlab = 'Sample size',
-     ylab = 'Time (s) for transferring data from HDFS to the master node', type = 'l', col
+
+par(ps = 12, cex = 1, cex.main = 1, mfrow = c(1, 2), mar = c(4, 4, 1, 1))
+
+## estimate the communication
+y = commcost[2:9, 2]
+x = commcost[2:9, 3]
+model = lm(y ~ x)
+
+overhead = commcost[1, 2] - commcost[2, 2]
+
+npar_oneshot = 44
+npartition = 117
+npar_dqr = npar_oneshot + npar_oneshot + 1 + npartition * npar_oneshot ^ 2 + npar_oneshot * npartition
+
+time_all = predict(model, newdata = data.frame(x = c(npar_oneshot, npar_dqr))) # + overhead
+names(time_all) = c("One Shot", "DQR")
+barplot(time_all, ylab = 'Total communication time (s)', ylim = c(0, 6))
+abline(h = overhead, col = "red", lwd = 2)
+legend('topright', legend = c("overhead"), col = "red", lty = "solid", box.col = "white", lwd = 2)
+
+commcost_overhead = commcost
+commcost_overhead[1, 2] = commcost_overhead[1, 2] - overhead
+
+plot(commcost_overhead$sample_size, commcost_overhead$time_commcost,
+     xlab = 'Object length',
+     ylab = 'One round communication time (s)', type = 'l', col
      = "blue", lwd = 2)
-points(commcost[1, 3], commcost[1, 2], col = 'red', lwd = 2)
+#points(commcost[1, 3], commcost[1, 2], col = 'red', lwd = 2)
+
 dev.copy2pdf(file = 'commcost.pdf')
--- projects/plot_used_car_data.R
+++ projects/plot_used_car_data.R
@@ -1,9 +1,18 @@
 Y = read.table('~/workspace/dqr/Y.csv', header = T)
 
 par(mfrow = c(1, 2), mar = c(4, 4, 0.5, 0), las = 1)
+library(moments)
 options(scipen=999)
 
+sk1 = skewness(exp(Y[,1]))
+sk2 = skewness(Y[,1])
+
+ku1 = kurtosis(exp(Y[,1]))
+ku2 = kurtosis(Y[,1])
+
 hist(exp(Y[, 1]), xlab = "Price (USD)", main = '', breaks = 500, ylab = '', probability = FALSE, xlim = c(0, 100000), ylim = c(0, 400000))
-#lines(density(exp(Y[, 1]), adjust = 2))
+legend("topright", legend = c(paste("Skewness:", round(sk1, 2)), paste("Kurtosis:", round(ku1, 2))), box.col = "white", col = c(2, 1))
+                                        #lines(density(exp(Y[, 1]), adjust = 2))
 hist(Y[, 1], xlab = "Log Price (USD)",  main = '', ylab = '', probability = FALSE)
+legend("topright", legend = c(paste("Skewness:", round(sk2, 2)), paste("Kurtosis:", round(ku2, 2))), box.col = "white", col = c(2, 1))
 #lines(density(Y[, 1]))
